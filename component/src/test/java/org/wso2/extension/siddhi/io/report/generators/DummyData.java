df370ea254fa25ea2e01046ca212c36e5b50ec7c
kts-desilva <ktsdesilva.15@cse,mrt.ac.lk>
2018-12-14 01:27:42 +0530

    Change class javadoc

--- component/src/test/java/org/wso2/extension/siddhi/io/report/generators/DummyData.java
+++ component/src/test/java/org/wso2/extension/siddhi/io/report/generators/DummyData.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Produces dummy data for the tests.
+ * Produce dummy data for the tests.
  */
 public class DummyData {
     public static final String DUMMY_PAYLOAD = "[{\"event\":{\"symbol\":\"WSO2\",\"price\":55.6,\"volume\":100}}," +
